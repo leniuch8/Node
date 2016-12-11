ca890b592ec5cd927cf8b4d030a0cbb5beeeab44
Paweł Płocharczyk <P.Plocharczyk.1@stud.elka.pw.edu.pl>
2016-12-11 22:14:01 +0100

    v1

---
+++ Node/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node
+{
+    class Node
+    {
+        public int Id
+        {
+            get; set;
+        }
+        public int listeningPort
+        {
+            get;
+        }
+        public int sendingPort
+        {
+            get;
+        }
+        private DataListener listener;
+        private DataSender sender;
+        public Node() { }
+        public Node(int Id, int listeningPort, int sendingPort)
+        {
+            this.Id = Id;
+            this.listeningPort = listeningPort;
+            this.sendingPort = sendingPort;
+            configuration();
+        }
+        private void configuration()
+        {
+            this.listener = new DataListener(this.listeningPort);
+            this.sender = new DataSender();
+        }
+        public static void onDataReceived(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ Node/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /** TODO: Wczytanie konfiguracji portow **/
+
+            // TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 1024); // nasz serwer
+            new Node(1, 1024,0);
+          //  TcpClient newClient = listener.AcceptTcpClient(); // akceptacja
+        //    BinaryReader reader = new BinaryReader(newClient.GetStream()); // a reader jest po stronie serwera
+         //   writer.Write(12345.15);
+
+            Console.Read();
+        }
+    }
+}
